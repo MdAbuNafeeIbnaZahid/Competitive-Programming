2b560ae9425069f85f6753c7c6e9fc28ce2ceb33
MdAbuNafeeIbnaZahid <1305023.maniz@ugrad.cse.buet.ac.bd>
2017-09-28 23:09:07 +0600

    solved an IDA* search problem

---
+++ UVA/A* or IDA* search/11212 - Editing a Book v4.cpp
@@ -0,0 +1,521 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+// Order Statistic Tree
+/* Special functions:
+
+		find_by_order(k) --> returns iterator to the kth largest element counting from 0
+		order_of_key(val) --> returns the number of items in a set that are strictly smaller than our item
+*/
+typedef tree<
+int,
+null_type,
+less<int>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+
+// Generic Pair with stream inserter
+template<class Typ1, class Typ2> class Pair
+{
+public:
+    Typ1 first;
+    Typ2 second;
+    Pair(){}
+    Pair(Typ1 first, Typ2 second)
+    {
+        this->first = first;
+        this->second = second;
+    }
+    bool operator < ( const Pair B ) const
+    {
+        if ( first == B.first )
+        {
+            return second < B.second;
+        }
+        return first < B.first;
+    }
+
+};
+template<class Typ1, class Typ2>
+ostream &operator << (ostream &stream, const Pair<Typ1, Typ2> pr)
+{
+    stream << " (" << pr.first << ", " << pr.second << ") ";
+    return stream;
+}
+
+
+// Generic Triple with stream inserter
+template<class Typ1, class Typ2, class Typ3> class Triple
+{
+public:
+    Typ1 first;
+    Typ2 second;
+    Typ3 third;
+    Triple(){}
+    Triple(Typ1 first, Typ2 second, Typ3 third)
+    {
+        this->first = first;
+        this->second = second;
+        this->third = third;
+    }
+    bool operator < ( const Triple B ) const
+    {
+        if ( first == B.first )
+        {
+            if ( second == B.second )
+            {
+                return third < B.third;
+            }
+            return second < B.second;
+        }
+        return first < B.first;
+    }
+
+};
+template<class Typ1, class Typ2, class Typ3>
+ostream &operator << (ostream &stream, const Triple<Typ1, Typ2, Typ3> tr)
+{
+    stream << " (" << tr.first << ", " << tr.second << ", " << tr.third << ") ";
+    return stream;
+}
+
+
+#define LL 			long long
+#define PairLL		Pair<long long, long long>
+#define TripleLL    Triple<long long, long long, long long>
+typedef vector<long long> VL;
+
+
+
+#define RESET(a) 		memset(a,0,sizeof(a))
+#define SET(a) 			memset(a,-1,sizeof(a))
+#define SORT(v)         sort(v.begin(),v.end())
+#define REV(v)          reverse(v.begin(),v.end())
+#define MP              make_pair
+#define PUB				push_back
+#define POB				pop_back
+#define PUF             push_front
+#define POF             pop_front
+
+
+#define FOR(i,a,b) for(i=(a);i<(b);i++)
+#define ROF(i,a,b) for(i=(a);i>(b);i--)
+
+#define DBG(x) cout<<#x<<" : "<<x<<endl
+#define DBG2(x, y) cout<<#x<<" : "<<(x)<<", "<<#y<<" : "<<(y)<<endl
+#define DBG3(x, y, z) cout<<#x<<" : "<<(x)<<", "<<#y<<" : "<<(y) << ", " << #z << " : " << (z) <<endl
+#define DBG4(a,b,c,d) cout<<#a<<" : "<<(a)<<", "<<#b<<" : "<<(b)<<", "<<#c<<" : "<<(c)<<", "<<#d<<": "<<(d)<<endl
+
+#define PI (2.0*acos(0.0)) //#define PI acos(-1.0)
+#define SPRIME 10007
+#define BPRIME 1000000007
+#define LLMAX ( (unsigned long long) -1LL >> 1LL )
+
+
+template<typename typ>
+void prVec( vector<typ> toPr )
+{
+    for (auto it : toPr)
+    {
+        cout << it << " , " << endl;
+    }
+    cout << endl;
+}
+
+template<typename typ>
+void prSet( set<typ> toPr )
+{
+    for (auto it : toPr)
+    {
+        cout << it << " , " << endl;
+    }
+    cout << endl;
+}
+
+template<typename T1, typename T2>
+void prMap( map<T1, T2> toPr )
+{
+    for (auto it : toPr)
+    {
+        cout << " [ " << it.first << " ] = " << it.second << endl;
+    }
+    cout << endl;
+}
+
+template<typename T1>
+void prAr(T1 *ar, LL si, LL ei)
+{
+    long long a, b, c;
+    for (a = si; a <= ei; a++)
+    {
+//        cout << "ar [ " << a << " ] = " << ar[a] << endl;
+        cout << ar[a] << " ";
+    }
+    cout << endl;
+}
+
+template<typename T1>
+void prMulSet( multiset<T1> toPr )
+{
+    for (auto it : toPr)
+    {
+        cout << it << " , " << endl;
+    }
+    cout << endl;
+}
+
+template<typename T1>
+void prQ( queue<T1> toPr )
+{
+    while( toPr.size() )
+    {
+        cout << toPr.front() << " , " << endl;
+        toPr.pop();
+    }
+    cout << endl;
+}
+
+template<typename T1>
+void prDQ( deque<T1> toPr )
+{
+    for (auto it : toPr)
+    {
+        cout << it << " , " << endl;
+    }
+    cout << endl;
+}
+
+void ILL(LL &u)
+{
+    LL a, b, c, d;
+    scanf("%lld", &u);
+}
+
+void ILL2(LL &u, LL &v)
+{
+    LL a, b, c, d, e;
+    scanf("%lld %lld", &u, &v);
+}
+
+void ILL3(LL &u, LL &v, LL &w)
+{
+    LL a, b, c, d, e;
+    scanf("%lld %lld %lld", &u, &v, &w);
+}
+
+void ILL4(LL &u, LL &v, LL &w, LL &x)
+{
+    LL a, b, c, d, e;
+    scanf("%lld %lld %lld %lld", &u, &v, &w, &x);
+}
+
+
+
+LL dirR[] = {1, -1, 0, 0};
+LL dirC[] = {0, 0, 1, -1};
+
+
+LL bigMod(LL a, LL b, LL M)
+{
+    if ( b == 0 ) return 1%M;
+    LL x = bigMod(a, b/2, M);
+    x = (x*x)%M;
+    if ( b&1LL )
+    {
+        x = (x*a)%M;
+    }
+    return x;
+}
+
+LL getGcd(LL u, LL v)
+{
+    if ( u > v )
+    {
+        swap(u, v);
+    }
+    if ( u == 0 )
+    {
+        return v;
+    }
+    return getGcd(v%u, u);
+}
+
+LL getLcm(LL u, LL v)
+{
+    LL g = getGcd(u, v);
+    return (u/g) * v;
+}
+
+template<typename T1, typename T2>
+T1 aMin(T1 &u, T2 v)
+{
+    if ( v < u )
+    {
+        u = v;
+    }
+    return u;
+}
+
+
+template<typename T1, typename T2>
+T1 aMax(T1 &u, T2 v)
+{
+    if ( v > u )
+    {
+        u = v;
+    }
+    return u;
+}
+
+
+
+struct DSU
+{
+    LL *parentAr, *countAr;
+    LL siz;
+    DSU(LL siz)
+    {
+        LL a, b, c,d ;
+        this->siz = siz;
+        parentAr = new LL[siz];
+        countAr = new LL[siz];
+        FOR(a,0,siz)
+        {
+            parentAr[a] = a;
+            countAr[a] = 1;
+        }
+    }
+    LL getParent( LL u )
+    {
+        LL ret;
+        if ( parentAr[u] != u )
+        {
+            parentAr[u] = getParent( parentAr[u] );
+        }
+        return parentAr[u];
+    }
+    void makePair(LL u, LL v)
+    {
+        if ( getParent(u) == getParent(v) )
+        {
+            return ;
+        }
+        countAr[ getParent(v) ] += countAr[ getParent(u) ];
+        parentAr[ getParent(u) ] = getParent( v );
+    }
+    LL getCount( LL u )
+    {
+        return countAr[ getParent(u) ];
+    }
+};
+
+
+struct BIT
+{
+    LL *treeAr;
+    LL siz;
+    BIT(){}
+    BIT(LL siz)
+    {
+        this->siz = siz;
+        treeAr = new long long[siz+9];
+        RESET( treeAr);
+    }
+    long long read(long long idx)
+    {
+        long long sum = 0;
+        while(idx > 0)
+        {
+            sum += treeAr[ idx ];
+            idx -= (idx & -idx);
+        }
+        return sum;
+    }
+    void update(long long idx, long long val)
+    {
+        while(idx<=siz)
+        {
+            treeAr[idx] += val;
+            idx += (idx & -idx);
+        }
+    }
+};
+
+
+// __builtin_popcountll = long long
+// void * memcpy ( void * destination, const void * source, size_t num );
+
+LL checkBit(LL num, LL pos)
+{
+    return (num>>pos) & 1LL;
+}
+
+LL setBit(LL num , LL pos)
+{
+    return num | (1LL<<pos);
+}
+
+LL resetBit(LL num, LL pos)
+{
+    return num & ( ~( 1LL<<pos ) );
+}
+
+
+
+
+/******   END OF HEADER *********/
+#define SIZE 19
+
+LL n;
+
+
+LL getSegCnt(LL ar[])
+{
+    LL a, b, c, d;
+    LL len = n;
+    LL ret = 0;
+    FOR( a, 0, len-1 )
+    {
+        ret += ( (ar[a]+1) != (ar[a+1]) );
+    }
+    return ret;
+}
+
+void changeAr(LL *ar, LL si, LL ei, LL change)
+{
+
+//    cout << "in changeAr " << endl;
+//    DBG3(si, ei, change);
+    LL a, b, c, d, e;
+    LL segSize = ei-si+1;
+//    DBG(segSize);
+    LL len = n;
+    LL newAr[len];
+    memcpy(newAr+si+change, ar+si, segSize*sizeof(LL) );
+//    cout << "copied the segment" << endl;
+//    prAr(newAr, 0, n-1);
+    LL  cur = 0;
+    FOR(a,0,len)
+    {
+        if ( a>=si && a<=ei )
+        {
+            continue;
+        }
+        while(cur>=si+change && cur<=ei+change)
+        {
+            cur++;
+        }
+        newAr[cur++] = ar[a];
+    }
+    memcpy(ar, newAr, n*sizeof(LL));
+}
+
+
+bool dfs(LL dep, LL prun, LL ar[])
+{
+//    cout << "in dfs " << endl;
+//    DBG2(dep, prun);
+    LL a, b, c, d, e, f;
+    LL len = n;
+//    DBG(len);
+//    prAr(ar, 0, n-1);
+    LL segCnt = getSegCnt(ar);
+//    DBG(segCnt);
+    if (segCnt==0)
+    {
+//        cout << " segCnt = 0 " << endl;
+//        DBG2(d, prun);
+        if ( dep<=prun )
+        {
+//            prAr(ar, 0, n-1);
+        }
+        return dep<=prun;
+    }
+    LL h = segCnt/3;
+//    DBG(h);
+    LL fVal = dep + h;
+//    DBG(fVal);
+    if ( 3*dep + segCnt > 3*prun )
+    {
+        return false;
+    }
+//    cout << " will generate children " << endl;
+    FOR(a,0,len)
+    {
+//        DBG(a);
+        FOR(b,a,len)
+        {
+//            DBG(b);
+            LL segSize = b-a+1;
+//            DBG(segSize);
+            FOR(c,0,len-segSize+1)
+            {
+//                DBG(c);
+                LL change = c-a;
+//                DBG(change);
+                LL newAr[len];
+                memcpy(newAr, ar, len*sizeof(LL));
+//                cout << "newAr is initiated " << endl;
+//                prAr(newAr, 0, n-1);
+                changeAr(newAr, a, b, change);
+//                cout << "newAr is changed " << endl;
+//                prAr(newAr, 0, n-1);
+                LL newH = getSegCnt(newAr)/3;
+                if ( getSegCnt(newAr) +3*dep+3 > 3*prun)
+                {
+                    continue;
+                }
+                bool res = dfs(dep+1, prun, newAr);
+                if (res)
+                {
+//                    prAr(ar, 0, n-1);
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+
+int main()
+{
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    LL a, b, c, d, e;
+    LL kase = 0;
+    while(++kase)
+    {
+//        DBG(kase);
+        ILL(n);
+//        DBG(n);
+        if (n==0)
+        {
+            return 0;
+        }
+        LL ar[n];
+        FOR(a,0,n)
+        {
+            ILL(ar[a]);
+        }
+//        prAr(ar, 0, n-1);
+        LL ans;
+        FOR(ans,0,n)
+        {
+            bool res = dfs(0, ans, ar);
+            if ( res )
+            {
+                break;
+            }
+        }
+        printf("Case %lld: %lld\n", kase, ans);
+    }
+    return 0;
+}
+
+
